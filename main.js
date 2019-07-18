27eadec415c6ba054c67cda388a81d62b7ee3a43
Symbolk <symbolk@163.com>
2019-07-18 15:24:39 +0800

    Quit and install.

--- main.js
+++ main.js
@@ -85,9 +85,11 @@ autoUpdater.on('download-progress', (progressObj) => {
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
   sendStatusToWindow(log_message);
 })
+
 autoUpdater.on('update-downloaded', (info) => {
-  sendStatusToWindow('Update downloaded');
-});
+  autoUpdater.quitAndInstall();  
+})
+
 app.on('ready', function() {
   // Create the Menu
   const menu = Menu.buildFromTemplate(template);
@@ -137,6 +139,4 @@ app.on('ready', function()  {
 // })
 // autoUpdater.on('download-progress', (progressObj) => {
 // })
-// autoUpdater.on('update-downloaded', (info) => {
-//   autoUpdater.quitAndInstall();  
-// })
+
